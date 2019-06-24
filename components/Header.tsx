50b141a703f32655b5dee239e1e9c4df9383a1c4
박민 <zooqzqz@gmail.com>
2019-06-24 17:26:59 +0900

    ENH: Header Full Body

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
-import { Menu } from "semantic-ui-react";
+import { Menu, Container } from "semantic-ui-react";
 
 import Link from "next/link";
 
 const Header = () => (
   <Menu tabular>
-    <Link href="/">
-      <Menu.Item name="home" />
-    </Link>
+    <Container>
+      <Link href="/">
+        <Menu.Item name="home" />
+      </Link>
+    </Container>
   </Menu>
 );
 
--- layouts/Default.tsx
+++ layouts/Default.tsx
@@ -1,25 +1,27 @@
-import { Container } from 'semantic-ui-react'
+import { Container } from "semantic-ui-react";
 
-import Header from '../components/Header'
+import Header from "../components/Header";
 
 interface IDefaultLayoutProps {
-    children: JSX.Element[]
+  children: JSX.Element[];
 }
 
 const DefaultLayout = (props: IDefaultLayoutProps) => (
+  <>
+    <Header />
     <Container>
-        <Header/>
-        {props.children}
-    <style jsx>{
-        `
-        div {
+      {props.children}
+      <style jsx>
+        {`
+          div {
             width: 720px;
             padding-right: 15px;
             padding-left: 15px;
-        }
+          }
         `}
-    </style>
+      </style>
     </Container>
-)
+  </>
+);
 
-export default DefaultLayout
+export default DefaultLayout;
